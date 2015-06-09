1dca0879efc22ff1f512b84dea68ee9466750895
Golam Hassan <golam@sols.co>
2015-06-09 18:02:48 -0400

    first commit

---
+++ src/Contract_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+
+describe 'Contract_setup' do
+
+  it 'should The Sols team can set up the contract to the provider' do
+
+    puts 'the provider can set up contract on the web'
+  end
+end
